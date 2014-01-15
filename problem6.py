6a2acff0af9c19bbbe5fb70e029c245786b6e2bb
Tom Rice <t.rice90@gmail.com>
2014-01-15 17:21:52 -0500

    Problem 6 solved!

--- problem6.py
+++ problem6.py
@@ -15,11 +15,25 @@ http://projecteuler.net/problem=6
 
 """
 
+import numpy as np
+
+def sum_of_squares_up_to(value):
+    """ Computes the sum of squares up to the input value. """
+
+    return np.sum(np.arange(value+1)**2)
+
+def square_of_sum_up_to(value):
+    """ Computes the square of the sum of all values up to the input. """
+
+    return np.sum(np.arange(value+1))**2
+    
+
 def diff_sumofsquares_squareofsum_up_to(value):
 
-    pass
+    return np.abs(sum_of_squares_up_to(value) - square_of_sum_up_to(value))
 
-
+assert sum_of_squares_up_to(10) == 385
+assert square_of_sum_up_to(10) == 3025
 assert diff_sumofsquares_squareofsum_up_to(10) == 2640
 
 print diff_sumofsquares_squareofsum_up_to(100)
