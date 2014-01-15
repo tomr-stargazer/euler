de09e66999c9e840ee0845524671a516a2c48cbc
Tom Rice <t.rice90@gmail.com>
2014-01-15 02:56:51 -0500

    Wrote a skeleton for this problem I think I just solved.

---
+++ problem5.py
@@ -0,0 +1,14 @@
+"""
+Something about evenly dividing things.
+
+"""
+
+from problem3 import find_largest_prime_factor
+
+def find_smallest_evenly_divided_number_for(value):
+
+    pass
+
+assert find_smallest_evenly_divided_number_for(10) == 2520
+
+print find_smallest_evenly_divided_number_for(20)
