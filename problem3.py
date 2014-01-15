2a0247f5e59dbc798a06692d21a4edc3c5f96d91
Tom Rice <t.rice90@gmail.com>
2014-01-15 01:33:23 -0500

    I marked this solution invalid, and will soon replace it with his evil twin.

--- problem3.py
+++ problem3.py
@@ -13,6 +13,8 @@ from __future__ import division
 
 import numpy as np
 
+print "This solution is too slow for the input -- see the other one in here."
+
 def is_prime(value):
     """ Checks if current value is prime. Returns True or False. """
 
@@ -24,16 +26,28 @@ def is_prime(value):
 
     return True
 
+# Revise this - use a sieve!
 def primes_up_to(value):
-    """ Generates a list of all primes less than or equal to `value`. """
+    """ 
+    Generates a list of all primes less than or equal to `value`. 
+
+    An implementation of the Sieve of Eratosthenes.
+    
+    """
 
     if value < 2: raise ValueError
-    
-    primes = []
 
-    for i in range(2, value+1): # the +1 is to include the input value!
-        if is_prime(i):
-            primes.append(i)
+    # Start with two and all the odd numbers.
+    possible_primes = [2] + range(3, value+1, 2)
+    primes = set(possible_primes)
+
+    for i in possible_primes:
+
+        if i in primes:
+
+            # Remove all the MULTIPLES of i (but not i itself!)
+            for j in range(2*i, value+1, i):
+                primes.discard(j)
 
     return primes
 
@@ -52,16 +66,13 @@ def prime_factors_of(value):
     # (don't optimize prematurely!)
 
     # WELP. I tried to generate all primes up to value//2! what a mistake.
+    # or was it? maybe it was just a bad implementation of prime-finding?
 
     factors = []
 
     for i in range(2, value//2):
         if value % i == 0:
             factors.append(i)
-
-    print factors
-
-    return
 
     prime_factors = []
     
@@ -74,6 +85,6 @@ def prime_factors_of(value):
 assert set(prime_factors_of(13195)) == set([5, 7, 13, 29])
 assert max(prime_factors_of(13195)) == 29
 
-assert primes_up_to(2) == [2]
+assert primes_up_to(2) == set([2])
 
-print max(prime_factors_of(600851475143))
+#print max(prime_factors_of(600851475143))
