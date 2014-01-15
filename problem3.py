4fdcf8d4a53b9263fe2c48f55a5f2248f7e582db
Tom Rice <t.rice90@gmail.com>
2014-01-14 21:45:55 -0500

    finished a solution that passes tests, but... NEVER USE IT.
    I implemented it badly! It will take forever! It's not even responding to CTRL-C!
    
    What I did wrong:
    
    my solution tries to
    *FIND ALL PRIMES UP TO (value)/2 *
    and *then*
    *CHECK TO SEE WHICH OF THOSE ARE FACTORS OF (value)*.
    
    The MUCH faster *equivalent* solution would be to
    *FIND ALL FACTORS OF (value)*
    and *then*
    *CHECK TO SEE WHICH OF THOSE ARE PRIME*.
    
    I may further optimize: start from the top and pick the first (i.e. largest).

--- problem3.py
+++ problem3.py
@@ -29,9 +29,9 @@ def primes_up_to(value):
 
     if value < 2: raise ValueError
     
-    primes = [2]
+    primes = []
 
-    for i in range(2, value):
+    for i in range(2, value+1): # the +1 is to include the input value!
         if is_prime(i):
             primes.append(i)
 
@@ -51,11 +51,19 @@ def prime_factors_of(value):
     # and just return the first one. we'll see if i need that optimization.
     # (don't optimize prematurely!)
 
-    pass
+    possible_prime_factors = primes_up_to(value//2)
 
-assert set(prime_factors_of(13195)) == set(5, 7, 13, 29)
+    prime_factors = []
+    
+    for i in possible_prime_factors:
+        if value % i == 0:
+            prime_factors.append(i)
+
+    return prime_factors
+    
+assert set(prime_factors_of(13195)) == set([5, 7, 13, 29])
 assert max(prime_factors_of(13195)) == 29
 
+assert primes_up_to(2) == [2]
 
-
-    
+print max(prime_factors_of(600851475143))
