1e18636374007de3704b4ad6b11453bab3d6a1de
Tom Rice <t.rice90@gmail.com>
2014-01-15 02:01:33 -0500

    This solution *really works*!!!! and is bloody fast and really simple!

--- problem3_alt.py
+++ problem3_alt.py
@@ -18,28 +18,37 @@ import numpy as np
 
 # and hack it down piece by piece.
 
-# first: go through a bunch 
-# If so, decimate evilnumber by that value (as many times as is possible) 
-#        and move onto the next iteratively-generated prime.
+# first: go through a bunch of numbers. Everytime you hit a divisor:
+#  decimate evilnumber by that value (as many times as is possible) 
+#  and then keep going til you've found all the factors 
+#  (i.e. the decimated number hits one) or you've checked 
+#  every factor up to sqrt(evilnumber).
+#  Then you're left with two numbers: the most recent divisor that worked,
+#  and the decimated number that remained. 
+#  They're both prime factors - return the larger of the two.
+#   (sometimes one of those two is actually 1, 
+#    in which case your input was prime and you're golden.)
+#   (this actually works as a pretty neat prime checker, too!)
 
 def find_largest_prime_factor(value):
     """ Finds the largest prime factor of a given number. """
-
-    current_largest_factor = 2
+    # I think it really works! and is somewhat optimal!
+    
+    current_largest_factor = 1
     n = 2
 
     current_value = value
 
-    while n < np.sqrt(value):
+    while n <= np.sqrt(value) and current_value > 1:
 
-        if current_value % n == 0:
+        while current_value % n == 0:
 
-            current_value = current_value/n
+            current_value = current_value//n
             current_largest_factor = n
             
         n += 1
 
-    return current_largest_factor
+    return max(current_largest_factor, current_value)
 
 assert find_largest_prime_factor(13195) == 29
 
