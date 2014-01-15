fb7605c09d39a4ff2a5af59debcde5c04ea0fb22
Tom Rice <t.rice90@gmail.com>
2014-01-15 17:09:32 -0500

    Made a skeletal problem6!

---
+++ problem6.py
@@ -0,0 +1,25 @@
+"""
+Sum square difference
+Problem 6
+The sum of the squares of the first ten natural numbers is,
+
+1**2 + 2**2 + ... + 10**2 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)**2 = 55**2 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+http://projecteuler.net/problem=6
+
+"""
+
+def diff_sumofsquares_squareofsum_up_to(value):
+
+    pass
+
+
+assert diff_sumofsquares_squareofsum_up_to(10) == 2640
+
+print diff_sumofsquares_squareofsum_up_to(100)
