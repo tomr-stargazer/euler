c6bcd01f3ad7b24312ed1ca8622948c1b5bd3077
Tom Rice <t.rice90@gmail.com>
2014-01-15 21:58:39 -0500

    Solved problem 10! That sieve is so useful!

--- problem10.py
+++ problem10.py
@@ -8,10 +8,26 @@ Find the sum of all the primes below two million.
 http://projecteuler.net/problem=10
 """
 
+import numpy as np
+
 def sum_primes_below(value):
     """ Sums all primes below an input value. """
 
-    pass
+    # let's try a sieve of Eratosthenes.
+    
+    possible_primes = [2] + range(3, value, 2)
+    primes = set(possible_primes)
+
+    for n in possible_primes:
+
+        if n in primes:
+
+            for kn in range(2*n, value, n):
+                primes.discard(kn)
+
+    print "There are %d primes below %d" % (len(primes), value)
+
+    return np.sum(list(primes))
 
 assert sum_primes_below(10) == 17
 
