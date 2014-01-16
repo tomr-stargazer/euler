2f91f2063a14f47c2380b23d8349c34b5e0733f9
Tom Rice <t.rice90@gmail.com>
2014-01-15 21:00:02 -0500

    Wrote a skeleton problem10.

---
+++ problem10.py
@@ -0,0 +1,18 @@
+"""
+Summation of primes
+Problem 10
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+
+http://projecteuler.net/problem=10
+"""
+
+def sum_primes_below(value):
+    """ Sums all primes below an input value. """
+
+    pass
+
+assert sum_primes_below(10) == 17
+
+print sum_primes_below(int(2e6))
